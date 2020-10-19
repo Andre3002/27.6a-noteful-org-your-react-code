c6d4bf3c6ed9ef8af4f400bd77991dbc59e27cb6
Andre <afowlk1@gmail.com>
2020-10-19 12:38:28 -0400

    Validation to ensure name in add note field is not blank: Complete

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react"
 import ApiContext from "../ApiContext"
 import config from "../config"
+import ValidationError from "../ValidationError/ValidationError"
 
 export default class AddNote extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: {
+        value: "",
+        touched: false,
+      },
+    }
+  }
+
+  updateName(name){
+    this.setState({name:{value:name, touched:true}})
+  }
+
+  validateName(){
+    const name = this.state.name.value.trim()
+    if (name.length === 0){
+      return "The name field can not be empty"
+    }
+  }
+
   static contextType = ApiContext
 
   onAddNote = (e) => {
@@ -10,7 +32,7 @@ export default class AddNote extends Component {
     const name = e.target.name.value
     const content = e.target.content.value
     const folderId = e.target.folderId.value
-    const note = { name, content, folderId}
+    const note = { name, content, folderId }
 
     fetch(`${config.API_ENDPOINT}/notes`, {
       method: "POST",
@@ -27,15 +49,29 @@ export default class AddNote extends Component {
     return (
       <form id="add-note" onSubmit={this.onAddNote}>
         <label id="label"> Name </label>
-        <input type="text" id="label" name="name" />
+        <input 
+          type="text" 
+          id="label" 
+          name="name"
+          onChange={(e)=> this.updateName(e.target.value)}
+          // defaultValue="New Note"
+          /> 
+          {this.state.name.touched && (<ValidationError message={this.validateName()}/>
+          )}
+        
         <label id="content"> Content </label>
         <input type="text" id="content" content="content" />
         <select name="folderId">
-            {
-                this.context.folders.map(folder=><option value={folder.id}>{folder.name}</option>)
-            }
+          {this.context.folders.map((folder) => (
+            <option value={folder.id}>{folder.name}</option>
+          ))}
         </select>
-        <button type="submit">submit</button>
+        <button 
+          type="submit"
+          disabled = {this.validateName()}
+          >
+            submit
+            </button>
       </form>
     )
   }
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { format } from 'date-fns'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import ApiContext from '../ApiContext'
-import config from '../config'
-import './Note.css'
+import React from "react"
+import { Link } from "react-router-dom"
+import { format } from "date-fns"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import ApiContext from "../ApiContext"
+import config from "../config"
+import "./Note.css"
 
 export default class Note extends React.Component {
-  static defaultProps ={
+  static defaultProps = {
     onDeleteNote: () => {},
   }
-  static contextType = ApiContext;
+  static contextType = ApiContext
 
-  handleClickDelete = e => {
+  handleClickDelete = (e) => {
     e.preventDefault()
     const noteId = this.props.id
 
     fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json",
       },
     })
-      .then(res => {
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e))
         return res.json()
       })
       .then(() => {
@@ -32,7 +31,7 @@ export default class Note extends React.Component {
         // allow parent to perform extra behaviour
         this.props.onDeleteNote(noteId)
       })
-      .catch(error => {
+      .catch((error) => {
         console.error({ error })
       })
   }
@@ -40,28 +39,21 @@ export default class Note extends React.Component {
   render() {
     const { name, id, modified } = this.props
     return (
-      <div className='Note'>
-        <h2 className='Note__title'>
-          <Link to={`/note/${id}`}>
-            {name}
-          </Link>
+      <div className="Note">
+        <h2 className="Note__title">
+          <Link to={`/note/${id}`}>{name}</Link>
         </h2>
         <button
-          className='Note__delete'
-          type='button'
+          className="Note__delete"
+          type="button"
           onClick={this.handleClickDelete}
         >
-          <FontAwesomeIcon icon='trash-alt' />
-          {' '}
-          remove
+          <FontAwesomeIcon icon="trash-alt" /> remove
         </button>
-        <div className='Note__dates'>
-          <div className='Note__dates-modified'>
-            Modified
-            {' '}
-            <span className='Date'>
-              {format(modified, 'Do MMM YYYY')}
-            </span>
+        <div className="Note__dates">
+          <div className="Note__dates-modified">
+            Modified{" "}
+            <span className="Date">{format(modified, "Do MMM YYYY")}</span>
           </div>
         </div>
       </div>
