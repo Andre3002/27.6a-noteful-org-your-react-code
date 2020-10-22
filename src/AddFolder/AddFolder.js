69622c7e54dcb532d4670bf901b7a3c5b5015286
Andre <afowlk1@gmail.com>
2020-10-22 10:54:04 -0400

    10.22 start

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -3,19 +3,16 @@ import ApiContext from "../ApiContext"
 import config from "../config"
 import "./AddFolder.css"
 
-
 export default class AddFolder extends Component {
   static contextType = ApiContext
- 
+
   onAddFolder = (e) => {
     e.preventDefault()
     const name = e.target.name.value
     const folder = { name }
     fetch(`${config.API_ENDPOINT}/folders`, {
       method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: {"content-type": "application/json",},
       body: JSON.stringify(folder),
     }).then((res) => {
       this.props.history.push("/")
