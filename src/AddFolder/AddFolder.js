699e63ef68ddefe7274106695c0d0be5cb25947d
Andre <afowlk1@gmail.com>
2020-10-22 11:02:55 -0400

    fixed folder empty name validation

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -2,8 +2,31 @@ import React, { Component } from "react"
 import ApiContext from "../ApiContext"
 import config from "../config"
 import "./AddFolder.css"
+import ValidationError from "../ValidationError/ValidationError"
+
 
 export default class AddFolder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: {
+        value: "",
+        touched: false,
+      },
+    }
+  }
+
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } })
+  }
+
+  validateName() {
+    const name = this.state.name.value.trim()
+    if (name.length === 0) {
+      return "The folder name can not be blank"
+    }
+  }
+
   static contextType = ApiContext
 
   onAddFolder = (e) => {
@@ -12,7 +35,7 @@ export default class AddFolder extends Component {
     const folder = { name }
     fetch(`${config.API_ENDPOINT}/folders`, {
       method: "POST",
-      headers: {"content-type": "application/json",},
+      headers: { "content-type": "application/json" },
       body: JSON.stringify(folder),
     }).then((res) => {
       this.props.history.push("/")
@@ -23,8 +46,18 @@ export default class AddFolder extends Component {
     return (
       <form id="add-folder" onSubmit={this.onAddFolder}>
         <label id="label"> Name </label>
-        <input type="text" id="label" name="name" />
-        <button type="submit">submit</button>
+        <input
+          type="text"
+          id="label"
+          name="name"
+          onChange={(e) => this.updateName(e.target.value)}
+        />
+        {this.state.name.touched && (
+          <ValidationError message={this.validateName()} />
+        )}
+        <button type="submit" disabled={this.validateName()}>
+          submit
+        </button>
       </form>
     )
   }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -4,7 +4,6 @@ import config from "../config"
 import ValidationError from "../ValidationError/ValidationError"
 import "./AddNote.css"
 
-
 export default class AddNote extends Component {
   constructor(props) {
     super(props)
@@ -16,13 +15,13 @@ export default class AddNote extends Component {
     }
   }
 
-  updateName(name){
-    this.setState({name:{value:name, touched:true}})
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } })
   }
 
-  validateName(){
+  validateName() {
     const name = this.state.name.value.trim()
-    if (name.length === 0){
+    if (name.length === 0) {
       return "The name field can not be empty"
     }
   }
@@ -52,16 +51,17 @@ export default class AddNote extends Component {
     return (
       <form id="add-note" onSubmit={this.onAddNote}>
         <label id="label"> Name </label>
-        <input 
-          type="text" 
-          id="label" 
+        <input
+          type="text"
+          id="label"
           name="name"
-          onChange={(e)=> this.updateName(e.target.value)}
+          onChange={(e) => this.updateName(e.target.value)}
           // defaultValue="New Note"
-          /> 
-          {this.state.name.touched && (<ValidationError message={this.validateName()}/>
-          )}
-        
+        />
+        {this.state.name.touched && (
+          <ValidationError message={this.validateName()} />
+        )}
+
         <label id="content"> Content </label>
         <input type="text" id="content" content="content" />
         <select name="folderId">
@@ -69,12 +69,9 @@ export default class AddNote extends Component {
             <option value={folder.id}>{folder.name}</option>
           ))}
         </select>
-        <button 
-          type="submit"
-          disabled = {this.validateName()}
-          >
-            submit
-            </button>
+        <button type="submit" disabled={this.validateName()}>
+          submit
+        </button>
       </form>
     )
   }
